c7a3c99306254e5ea269d4960b3201d59a4d3cb8
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-03 11:14:01 -0700

    added additional logic to check for blank or null names

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("I'm loaded!")
 
 var startButton = $('.newgame')
-var display = $('.display')
+var display = $('.container')
 var playerOne = {
   name: '',
   score: 0
@@ -27,31 +27,42 @@ function newGame(){
 }
 
 
-//As a user, I should see instructions on how to play the game after clicking the new game button
 
-function instructions(){
-
-  $('.instruct').html('<p> This is my instructions for the game</p>' + '<button class="instruct-button">OK</button>')
-
-  $('.instruct-button').on('click', function(){
-    $('.instruct').hide()
-  })
-
-
-}
 
 // As a user, I should be able to input my name so that I can know who I'm playing against.
 
 function namePrompt(){
-  playerOne.name = prompt("What is player One's name")
-  playerTwo.name = prompt("What is player Two's name")
+
+
+  function playerOneName(){
+    playerOne.name = prompt("What is player One's name")
+
+  if (!playerOne.name || playerOne.name === ""){
+    alert('Player One Name cannot be blank. Please put in a name')
+    playerOneName();
+    }
+  }
+
+  function playerTwoName(){
+    playerTwo.name = prompt("What is player Two's name")
+
+  if (!playerTwo.name || playerTwo.name === ""){
+    alert('Player Two Name cannot be blank. Please put in a name')
+    playerOneName();
+    }
+  }
+
+playerOneName()
+playerTwoName()
 }
 
 
 //Also, I need to make sure the name's are correct because well, people make mistakes.
 function nameCheck(){
 
-  $('.instruct').html('<p>Are these names correct?' + '<br>'+ playerOne.name + '<br>'+ playerTwo.name +'</p>' + '<button class="instruct-button yes ">Yes</button> <button class="instruct-button no">No</button>')
+  $('.instruct').html('<p>Are these names correct?' + '<br>' + '<br>'+ playerOne.name + '<br>'+ playerTwo.name +'</p>' + '<button class="instruct-button yes ">Yes</button> <button class="instruct-button no">No</button>')
+
+
 
   var no = $('.no')
   var yes =$('.yes')
@@ -68,4 +79,17 @@ function nameCheck(){
     console.log("You've clicked yes!")
     instructions()
   })
+}
+
+//As a user, I should see instructions on how to play the game after clicking the new game button
+
+function instructions(){
+
+  $('.instruct').html('<p> This is my instructions for the game</p>' + '<button class="instruct-button">OK</button>')
+
+  $('.instruct-button').on('click', function(){
+    $('.instruct').hide()
+  })
+
+
 }
