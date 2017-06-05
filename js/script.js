a56ea5d9fdd9847109a56c780b4c4fd805928e62
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-05 11:17:54 -0700

    moved position of hero-box to bottom right corner

--- js/script.js
+++ js/script.js
@@ -7,20 +7,20 @@ var playerOne = {
   score: 0
 }
 
-var playerTwo= {
+var playerTwo = {
   name: '',
   score: 0
 }
 
 //As a user, I should be able to start a new game by clicking a button
 
-startButton.on('click', function(){
+startButton.on('click', function() {
   newGame()
 
 })
 
 
-function newGame(){
+function newGame() {
   namePrompt()
   nameCheck()
 }
@@ -30,53 +30,53 @@ function newGame(){
 
 // As a user, I should be able to input my name so that I can know who I'm playing against.
 
-function namePrompt(){
+function namePrompt() {
 
 
-  function playerOneName(){
+  function playerOneName() {
     playerOne.name = prompt("What is player One's name")
 
-  if (!playerOne.name || playerOne.name === ""){
-    alert('Player One Name cannot be blank. Please put in a name')
-    playerOneName();
+    if (!playerOne.name || playerOne.name === "") {
+      alert('Player One Name cannot be blank. Please put in a name')
+      playerOneName();
     }
   }
 
-  function playerTwoName(){
+  function playerTwoName() {
     playerTwo.name = prompt("What is player Two's name")
 
-  if (!playerTwo.name || playerTwo.name === ""){
-    alert('Player Two Name cannot be blank. Please put in a name')
-    playerOneName();
+    if (!playerTwo.name || playerTwo.name === "") {
+      alert('Player Two Name cannot be blank. Please put in a name')
+      playerOneName();
     }
   }
 
-playerOneName()
-playerTwoName()
+  playerOneName()
+  playerTwoName()
 }
 
 
 //Also, I need to make sure the name's are correct because well, people make mistakes.
-function nameCheck(){
+function nameCheck() {
 
-    display.append('<div class = "instruct"><div</>')
+  display.append('<div class = "instruct"><div</>')
 
-  $('.instruct').html('<p>Are these names correct?' + '<br>' + '<br>'+ playerOne.name + '<br>'+ playerTwo.name +'</p>' + '<button class="instruct-button yes ">Yes</button> <button class="instruct-button no">No</button>')
+  $('.instruct').html('<p>Are these names correct?' + '<br>' + '<br>' + playerOne.name + '<br>' + playerTwo.name + '</p>' + '<button class="instruct-button yes ">Yes</button> <button class="instruct-button no">No</button>')
 
 
 
   var no = $('.no')
-  var yes =$('.yes')
+  var yes = $('.yes')
 
 
-  no.on('click', function(){
+  no.on('click', function() {
     console.log("You've clicked no!")
     namePrompt()
     nameCheck()
 
   })
 
-  yes.on('click', function(){
+  yes.on('click', function() {
     console.log("You've clicked yes!")
     instructions()
   })
@@ -84,22 +84,22 @@ function nameCheck(){
 
 //As a user, I should see instructions on how to play the game after clicking the new game button
 
-function instructions(){
-var instructions = "A fierce and wild monster rules these lands. Can you defeat the monster in 30 seconds or less?  "
-  $('.instruct').html('<p>' +instructions+'</p>' + '<button class="instruct-button">OK</button>')
+function instructions() {
+  var instructions = "A fierce and wild monster rules these lands. Can you defeat the monster in 30 seconds or less?  "
+  $('.instruct').html('<p>' + instructions + '</p>' + '<button class="instruct-button">OK</button>')
 
-  $('.instruct-button').on('click', function(){
+  $('.instruct-button').on('click', function() {
     $('.instruct').hide()
   })
 }
 
 //being able to see the enemy.
-function boss(){
+function boss() {
   //target the display div and append the image.
 
   display.append("<img src='img/cyclops.png' class='enemy'>")
 
-  $('img.enemy').on('click', function(){
+  $('img.enemy').on('click', function() {
     console.log("I've been clicked")
     $(this).fadeOut();
   })
@@ -107,44 +107,53 @@ function boss(){
 
 //being able to see my character on screen.
 function hero(){
-  display.append("<img src='img/hero.png' class='hero'>")
+  display.append("<div class='hero-box'><img src='img/hero.png' class='hero'><div>")
 
-  $('img.hero').on('click', function(){
+  $('img.hero').on('click', function() {
     console.log("I've been clicked")
     $(this).fadeOut();
   })
 }
 
-function attack(){
-     display.append("<button class='attack-normal'>ATTACK</button>")
-
-      display.append("<button class='attack-strong'>STRONG ATTACK!</button>")
-
-      var countToMega = 0
-
-     $('.attack-normal').on('click', function(){
-       console.log("I've been clicked")
-       playerOne.score +=10;
-     })
-
-     $('.attack-strong').on('click', function(){
-       console.log("I've been clicked")
-       playerOne.score +=30;
-       countToMega+=1
-
-       if (countToMega === 5){
-         console.log("MY ULT IS CHARGED")
-         display.append("<button class='attack-mega'>Mega Attack!</button>")
-         playerOne.score +=100
-         countToMega = 0;
-         $('.attack-mega').on('click', function(){
-           $(this).hide();
-         })
-       }
-     })
 
 
 
+
+function attack() {
+  display.append("<button class='attack-normal attack-button'>ATTACK</button>")
+
+  display.append("<button class='attack-strong attack-button'>STRONG ATTACK!</button>")
+
+
+
+
+
+  var countToMega = 0
+
+  $('.attack-normal').on('click', function() {
+    console.log("I've been clicked")
+    playerOne.score += 10;
+  })
+
+  $('.attack-strong').on('click', function() {
+    console.log("I've been clicked")
+    playerOne.score += 30;
+    countToMega += 1
+
+    if (countToMega === 5) {
+      console.log("MY ULT IS CHARGED")
+      display.append("<button class='attack-mega attack-button'>Mega Attack!</button>")
+      playerOne.score += 100
+      countToMega = 0;
+      $('.attack-mega').on('click', function() {
+        $(this).hide();
+      })
+    }
+  })
+
+  function randomMove(){
+    $()
+  }
 
 }
 
