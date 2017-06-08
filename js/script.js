af60d8c9dad6d78aa93bb955781d3ea11fc2bbf2
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-08 09:07:01 -0700

    refactor code due to win condition not updating. created gameState function and move win conditionals there

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Jquery selections
 var startButton = $('.newgame')
 var display = $('.container')
-var heroBox = $('.heroBox')
+
 
 //player and boss objects
 var playerOne = {
@@ -32,6 +32,7 @@ var currentPlayer = playerOne;
 var chargeCounter = 0
 var deathBlowCounter = 0
 var timeout = 2500;
+
 
 // random function for attacks, moving buttons, etc.
 function random(min, max) {
@@ -127,185 +128,85 @@ function instructions() {
 }
 
 
-function timer (){
+
+function timer() {
   milliseconds++
-  if (milliseconds === 999){
+  if (milliseconds === 999) {
     milliseconds = 0
-    seconds +=1
+    seconds += 1
   }
-  if (seconds === 60 ){
+  if (seconds === 60) {
 
     milliseconds = 0
     seconds = 0
   }
-  $('.timer').text('Time Elapsed: ' + seconds + ":" + milliseconds )
+  $('.timer').text('Time Elapsed: ' + seconds + ":" + milliseconds)
 }
+
+// random damage generator.
+function damage(min, max) {
+  var ap = random(min, max)
+  console.log(ap)
+  display.append("<h1 class='hit>" + ap + "</h1>")
+  gameState(ap)
+}
+
+
+//updates score
+function gameState(ap) {
+    currentPlayer.score += ap
+    boss.hp -= ap
+    var change = false;
+    $('.score').text("Player Score: " + currentPlayer.score)
+    $('.health').text("Boss Health: " + boss.hp)
+
+    if (boss.hp <= 1 && currentPlayer.hasGone===false) {
+        //clear the gameboard and save stats
+        console.log('the boss has been defeated!')
+        currentPlayer.timeElapsed = seconds/1000 + milliseconds
+        console.log(currentPlayer.name + "time's was: " +currentPlayer.timeElapsed)
+        currentPlayer.hasGone = true;
+        currentPlayer = playerTwo
+        change = true;
+        hargeCounter = 0
+        deathBlowCounter = 0
+        timeout = 2500;
+        boss.hp= 1000;
+        display.empty()
+      }
+
+        if (change===true && playerTwo.hasGone === true && playerOne.hasGone===true){
+          display.html("<h1>Game Over, both players have gone/h1>")
+        }
+
+
+}
+
+
 
 
 function gameboard() {
-  display.append('<div class = scoreDisplay><p class = "score"> Player Score: ' + currentPlayer.score + '</p>' + '<p class = "health">Boss Health: ' + boss.hp + '</p>' + '<p class = "timer">'+ '</p>' +'</div>');
-  display.append("<img src='img/cyclops.png' class='boss'>")
-  display.append("<div class='heroBox'><img src='img/hero.png' class='hero'><div>")
-  attack()
-  time = setInterval(timer, 1)
+    display.append('<div class = scoreDisplay><p class = "score"> Player Score: ' + currentPlayer.score + '</p>' + '<p class = "health">Boss Health: ' + boss.hp + '</p>' + '<p class = "timer">' + '</p>' + '</div>');
+    display.append("<img src='img/cyclops.png' class='boss'>")
+    display.append("<div class='heroBox'><img src='img/hero.png' class='hero'><div>")
+    $(".heroBox").append("<button class='attack-normal attack-button'></button>")
+    $(".heroBox").append("<button class='attack-strong attack-button'></button>")
+    setInterval(timer, 1)
+
+    $('.attack-normal').on('click', function() {
+      var normalAttack = gameState(25, 50);
+      $(this).css({
+        'left': random(0, 300) + "px",
+        'top': random(0, 125) + "px"
+      })
+    })
+
+    $('.attack-strong').on('click', function() {
+      var strongAttack = damage(50, 100)
+      $(this).css({
+        'left': random(0, 300) + "px",
+        'top': random(0, 125) + "px"
+      })
+
+    })
 }
-
-//I should be able to attack the boss.
-function attack() {
-  $(".heroBox").append("<button class='attack-normal attack-button'></button>")
-  $(".heroBox").append("<button class='attack-strong attack-button'></button>")
-
-  //updates score
-  function updateScore(ap) {
-    if (ap <= 0) {
-      boss.hp += 100;
-      $('.health').text("Boss health: " + boss.hp)
-    } else {
-      currentPlayer.score += ap
-      boss.hp -= ap
-      $('.score').text("Player Score: " + currentPlayer.score)
-      $('.health').text("Boss Health: " + boss.hp)
-    }
-  }
-
-
-  // random damage generator.
-  function damage(min, max) {
-
-    var ap = random(min, max)
-
-    function hitOrMiss() {
-      var hit = random(1, 5)
-      return hit;
-    }
-
-    if (hitOrMiss() <= 2) {
-      ap = 0;
-      console.log(ap)
-      $('.miss').text("MISS!")
-    } else {
-      console.log(ap)
-      display.append("<h1 class='hit>" + ap + "</h1>")
-      updateScore(ap)
-    }
-    return ap
-  }
-
-
-
-
-
-  $('.attack-normal').on('click', function() {
-    var normalAttack = updateScore(25, 50);
-    $(this).css({
-      'left': random(0, 300) + "px",
-      'top': random(0, 125) + "px"
-    })
-  })
-
-  $('.attack-strong').on('click', function() {
-    var strongAttack = damage(50, 100)
-    if (strongAttack > 0) {
-      chargeCounter += 1
-    } else {
-      console.log("you missed")
-    }
-    $(this).css({
-      'left': random(0, 300) + "px",
-      'top': random(0, 125) + "px"
-    })
-
-
-
-
-
-
-
-
-  })
-
-  if (chargeCounter === 5) {
-    $('.heroBox').append("<button class='attack-mega attack-button'></button>")
-    $('.attack-mega').fadeOut(timeout)
-    $('.attack-mega').on('click', function() {
-     var megaAttack = damage(200, 400)
-      $(this).hide();
-      timeout -= 300
-      if (megaAttack != 0){
-        deathBlowCounter += 1
-      } else {
-        console.log('missed DeathBlow')
-      }
-    })
-    chargeCounter = 0;
-  }
-
-  if (deathBlowCounter === 3) {
-    $('.heroBox').append("<button class='deathBlow attack-button'>Death Blow</button>")
-    $('.deathBlow').fadeOut(1000)
-    $('.deathBlow').on('click', function() {
-      var deathBlowChance = random(1,4);
-      if (deathBlowChance > 1) {
-        boss.hp = 0
-        boss.isDefeated = true
-        console.log("he's dead Jim")
-      } else {
-        console.log("Oh no! DeathBlow failed! boss's recovering half their health. ")
-        boss.hp += boss.hp/2
-      }
-
-    })
-
-  }
-
-    //reset game and win condition!
-
-
-
-    if (boss.hp < 1) {
-      //clear the gameboard and save stats
-      console.log('the boss has been defeated!')
-      currentPlayer.timeElapsed = seconds/1000 + milliseconds
-      console.log(currentPlayer.name + "time's was: " +currentPlayer.timeElapsed)
-      display.empty()
-
-      setTimeout(function(){
-        console.log("this is where you'd show stats...")
-      }, 1000)
-      currentPlayer.hasGone = true;
-
-    }
-
-
-    if (currentPlayer.hasGone === true){
-      currentPlayer = playerTwo
-      console.log("now we're clearing the variables")
-      chargeCounter = 0
-      deathBlowCounter = 0
-      timeout = 2500;
-      console.log('Now the current player is: '+ currentPlayer.name)
-      boss.hp = 1000
-      console.log("now it's " + currentPlayer.name + " 's turn!")
-      setTimeout(function(){
-        gameboard()
-      }, 5000)
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
