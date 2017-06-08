aea7400d8ecd4ae21e73f0066dd2c0217e3b5c3a
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-08 16:44:33 -0700

    fixed typo

--- js/script.js
+++ js/script.js
@@ -285,7 +285,7 @@ function gameboard() {
       console.log('Missed')
       chargeCounter = 0
   } else {
-    var megaAttack = damage(1git00, 400)
+    var megaAttack = damage(100, 400)
     deathBlowCounter+=1
     chargeCounter = 0
   }
@@ -296,6 +296,7 @@ function gameboard() {
     })
     $(this).hide()
   })
+}
 
   $('.deathBlow').on('click', function() {
     var chanceToHit = random (1,4)
